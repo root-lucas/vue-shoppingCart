d9e080cce73a20ba7728e8e4f5e5eb437aca601b
root-lucas <945832032@qq.com>
2019-09-29 10:53:25 +0800

    modify-cart

--- js/cart.js
+++ js/cart.js
@@ -1,82 +1,83 @@
-Vue.prototype.$http = axios
-var vm = new Vue({
-    el:"#app",
-    data:{
-        productList:[],
-        totalMoney:0,
-        checkAllFlag:false
+﻿var vm = new Vue({
+  el: "#app",
+  data: {
+    totalMoney: 0,
+    productList: [],
+    checkAllFlag: false,
+    delFlag: false,
+    curProduct: ""
+  },
+  filters: {
+    formatMoney: function(value) {
+      // value指当前调用过滤器传过来的值
+      return "¥" + value.toFixed(2);
+    }
+  },
+  mounted: function() {
+    this.$nextTick(function() {
+      vm.cartView();
+    });
+  },
+  methods: {
+    cartView: function() {
+      _this = this;
+      this.$http.get("data/cartData.json", { id: 123 }).then(function(res) {
+        this.productList = res.body.result.list;
+      });
     },
-    mounted:function () {
-        this.init()
-    },
-    computed:{
-        totalMoneyByComputed:function(){
-            let totalMoney = 0;
-            this.productList.forEach(item=>{
-                if(item.checked){
-                    totalMoney += item.price * item.count;
-                }
-            })
-            return totalMoney;
+    changeMoney: function(prodcut, May) {
+      if (May > 0) {
+        prodcut.productQuantity++;
+      } else {
+        prodcut.productQuantity--;
+        if (prodcut.productQuantity < 1) {
+          prodcut.productQuantity = 1;
+        }
       }
     },
-    watch:{
-        productList:{
-            handler:function () {
-                this.totalMoney = 0;
-                this.productList.forEach(item=>{
-                    if(item.checked){
-                        this.totalMoney += item.price * item.count;
-                    }
-                })
-            },
-            deep:true
-        }
+    selectProduct: function(item) {
+      if (typeof item.checked == "undefined") {
+        Vue.set(item, "checked", true);
+        // 局部注册
+        // this.$set(item, "checked", true);
+      } else {
+        item.checked = !item.checked;
+      }
+      this.calcTotalPrice();
     },
-    filters:{
-        formatMoney(value,type){
-            if(!type){
-                type = '';
-            }
-            return "￥"+value+type;
+    checkAll: function(flag) {
+      this.checkAllFlag = flag;
+      var _this = this;
+      this.productList.forEach(function(item, index) {
+        if (typeof item.checked == "undefined") {
+          _this.$set(item, "checked", (_this.checkAllFlag = flag));
+        } else {
+          item.checked = _this.checkAllFlag;
         }
+      });
+      this.calcTotalPrice();
     },
-    methods:{
-        init:function(){
-            axios.get("data/cart.json",{"id":123}).then(res=>{
-                this.productList = res.data.result.list;
-                this.totalMoney = res.data.result.totalMoney;
-            })
-
-        },
-        changeCount:function(item,count){
-            item.count += count;
-            if(item.count <= 0){
-                item.count = 1;
-            }
-        },
-        selectProduct:function (item) {
-            if(typeof item.checked == 'undefined'){
-                this.$set(item,"checked",true);
-            }else{
-                item.checked = !item.checked;
-            }
-        },
-        checkAll:function () {
-            this.checkAllFlag = !this.checkAllFlag;
-            this.productList.forEach(item=>{
-                if(typeof item.checked == 'undefined'){
-                    this.$set(item,"checked",this.checkAllFlag);
-                }else{
-                    item.checked = this.checkAllFlag;
-                }
-            })
-        },
-        deleteItem:function (index) {
-            let res = confirm("确定要删除吗？");
-            if(res){
-                this.productList.splice(index,1);
-            }
+    calcTotalPrice: function() {
+      var _this = this;
+      _this.totalMoney = 0;
+      this.productList.forEach(function(item, index) {
+        if (item.checked) {
+          _this.totalMoney += item.productPrice * item.productQuantity;
         }
+      });
+    },
+    delConfirm: function(item) {
+      this.delFlag = true;
+      this.curProduct = item;
+    },
+    delPorduct: function () {
+     var index = this.productList.indexOf(this.curProduct);
+     this.productList.splice(index, 1)
+     this.delFlag = false
     }
+  }
 });
+// 全局过滤器
+Vue.filter('Money',function(value,type) {
+        return "¥" + value.toFixed(2) + type;
+})
